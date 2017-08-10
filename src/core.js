d2f1eef468fbd0a51a56fe1c5d6432b88f7e092f
Roberto Dip <dip.jesusr@gmail.com>
2017-08-10 14:46:15 -0300

    ES2015fy the code

--- src/core.js
+++ src/core.js
@@ -1,50 +1,39 @@
-var helpers = require('./helpers');
-var core = {};
-var SCRIPT_RXP =
+const helpers = require('./helpers');
+const ASSET_REGEXP =
   /<script.*?src=".*?".*?\/script>|<link.*?rel="stylesheet".*?href=".*?".*?>/img
 
-module.exports = core;
+module.exports = {
+  replaceTags: async function(html, assetPath) {
+    let inlinedHtml = html;
 
-core.findTags = function(html) {
-  return html.match(SCRIPT_RXP);
-}
-
-core.replaceTags = function(html, tags, assetPath, callback) {
-  var inlinedHtml = html;
-
-  helpers.asyncForEach(tags, function(tag, iteratorCallback) {
-    var tagSrc = assetPath + '/' + getTagSrc(tag);
-
-    helpers.readFile(tagSrc, function(fileContents) {
-      var replacement = generateTag(tag, fileContents);
+    for(let tag of findTags(html)) {
+      const tagSrc = `${assetPath}/${getTagSrc(tag)}`;
+      const fileContents = await helpers.readFile(tagSrc);
+      const replacement = generateTag(tag, fileContents);
 
       inlinedHtml = inlinedHtml.replace(tag, replacement);
-      iteratorCallback();
-    });
+    }
 
-  }, function() {
-    callback(inlinedHtml);
-  });
+    return inlinedHtml;
+  }
+}
+
+function findTags(html) {
+  return html.match(ASSET_REGEXP);
 }
 
 function generateTag(tag, contents) {
-  var wrapper = tag.indexOf('script') !== -1 ? 'script' : 'style';
+  const wrapper = tag.indexOf('script') !== -1 ? 'script' : 'style';
 
-  return (
-    '<' + wrapper + '>' +
-      contents.trim() +
-    '</' + wrapper + '>'
-  );
+  return `<${wrapper}>${contents.trim()}</${wrapper}>`;
 }
 
 function getTagSrc(tag) {
-  var tagSrc = tag.match(/(src|ref)="(.*?)"/);
+  const tagSrc = tag.match(/(src|ref)="(.*?)"/);
 
   if (tagSrc) {
     return tagSrc[2];
   } else {
     return Error('Levin couldn\'t find a source for the tag: ' + tag);
   }
-
-  return src;
 }
